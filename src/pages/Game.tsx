01400ca9c8540ad9ad6670513bca12898f9edd34
Juan LB <ljuan7397@gmail.com>
2023-10-08 22:35:23 +0200

    Create new game page

---
+++ src/pages/Game.tsx
@@ -0,0 +1,13 @@
+// import { useSelector } from "react-redux"
+import { Board } from "../components/Board"
+// import { RootState } from "../store/store"
+
+export const Game = () => {
+    // const username = useSelector((state: RootState) => state.player.username)
+
+    return (
+        <div>
+            <Board />
+        </div>
+    )
+}
