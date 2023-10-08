cc2212c8b022eb7297ab89f76a9f759f72fb80eb
Juan LB <ljuan7397@gmail.com>
2023-10-09 00:27:15 +0200

    Create ranking basic page

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -1,7 +1,62 @@
+import { useSelector } from "react-redux"
+import { RootState } from "../store/store"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
 export const Ranking = () => {
+    const player = useSelector((state: RootState) => state.player)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (player.id === '') {
+            navigate('/')
+        }
+    })
+
     return (
-        <div>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '40px',
+        }}>
             <h1>Ranking</h1>
-        </div>
+            <div style={{
+                display: 'flex',
+                gap: '40px',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+            }}>
+                <StatBlock
+                    title='Victorias'
+                    value={player.wins}
+                />
+                <StatBlock
+                    title='Derrotas'
+                    value={player.losses}
+                />
+                <StatBlock
+                    title='Empates'
+                    value={player.draws}
+                />
+            </div>
+        </div >
     )
 }
+
+const StatBlock = ({
+    title,
+    value
+}: {
+    title: string;
+    value: string | number;
+}) => (
+    <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '16px',
+    }}>
+        <h2>{title}</h2>
+        <h3>{value}</h3>
+    </div>
+);
