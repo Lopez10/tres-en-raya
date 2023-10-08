7cc478235cafa9bb72882931ad34baa8f1cac639
Juan LB <ljuan7397@gmail.com>
2023-10-08 10:14:31 +0200

    Create Square component

---
+++ src/components/Square.tsx
@@ -0,0 +1,15 @@
+export const Square = ({
+    children,
+    updateBoard,
+    index
+  }:{
+    children: string,
+    updateBoard: (index: number) => void,
+    index: number
+  }) => {
+    return (
+      <button className='square' onClick={() => updateBoard(index)}>
+        {children}
+      </button>
+    )
+  }
