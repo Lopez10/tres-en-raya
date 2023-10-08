21be8c674efbd3ac6ce1178f7a958060a619cc99
Juan LB <ljuan7397@gmail.com>
2023-10-09 00:47:17 +0200

    Create back button component

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
+export const BackButton = () => {
+    const navigate = useNavigate();
+    return (
+        <button
+            style={{
+                fontSize: '1rem',
+            }}
+            onClick={() => navigate(-1)}
+        >
+            Volver
+        </button>
+    );
+}
