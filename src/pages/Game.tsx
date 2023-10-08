cb67c80cbc637fabd2b02fd486a35250670b8a8d
Juan LB <ljuan7397@gmail.com>
2023-10-08 23:36:48 +0200

    Create game page

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,10 +1,27 @@
-// import { useSelector } from "react-redux"
+import axios from "axios"
+import { useDispatch, useSelector } from "react-redux"
 import { Board } from "../components/Board"
-// import { RootState } from "../store/store"
+import { RootState } from "../store/store"
+import { setGame } from "../store/features/gameSlice"
+import { useEffect } from "react"
 
 export const Game = () => {
-    // const username = useSelector((state: RootState) => state.player.username)
+    const playerId = useSelector((state: RootState) => state.player.id)
+    const dispatch = useDispatch()
 
+    useEffect(() => {
+        const createEmptyGame = async () => {
+            const { data } = await axios.post('http://localhost:3000/games', {
+                playerId
+            }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            dispatch(setGame({ game: data }))
+        }
+        createEmptyGame()
+    }, [dispatch, playerId])
     return (
         <div>
             <Board />
