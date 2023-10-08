df0552f5e982a1e0c5032b41fab61fff6ed9bc34
Juan LB <ljuan7397@gmail.com>
2023-10-09 00:27:01 +0200

    Navigate if playerId not exist

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -4,24 +4,31 @@ import { Board } from "../components/Board"
 import { RootState } from "../store/store"
 import { setGame } from "../store/features/gameSlice"
 import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 export const Game = () => {
     const playerId = useSelector((state: RootState) => state.player.id)
     const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const createEmptyGame = async () => {
+        const { data } = await axios.post('http://localhost:3000/games', {
+            playerId
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        dispatch(setGame({ game: data }))
+    }
 
     useEffect(() => {
-        const createEmptyGame = async () => {
-            const { data } = await axios.post('http://localhost:3000/games', {
-                playerId
-            }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            dispatch(setGame({ game: data }))
+        if (playerId === '') {
+            navigate('/')
+        } else {
+            createEmptyGame()
         }
-        createEmptyGame()
-    }, [dispatch, playerId])
+    }, [dispatch, navigate, playerId])
     return (
         <div>
             <Board />
