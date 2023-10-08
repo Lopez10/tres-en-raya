e2d98571662ca670c63fe8c6cf6201afe17f4a58
Juan LB <ljuan7397@gmail.com>
2023-10-08 19:01:19 +0200

    Configure basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { UsernameForm } from './components/UsernameForm'
+import { Ranking } from './components/Ranking'
 
 function App() {
   return (
     <main className='board'>
       <h1>Tres en raya</h1>
-      <UsernameForm />
+      <Routes>
+        <Route path='/' element={<UsernameForm />} />
+        <Route path='/ranking' element={<Ranking />} />
+        <Route path='/game' element={<div>Game</div>} />
+      </Routes>
     </main>
   )
 }
