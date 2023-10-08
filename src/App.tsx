b6543a1b52afebe976fdacafbc64b4ca32b10368
Juan LB <ljuan7397@gmail.com>
2023-10-09 00:47:33 +0200

    Modify styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { UsernameForm } from './pages/UsernameForm'
-import { Ranking } from './components/Ranking'
+import { Ranking } from './pages/Ranking'
 import { Game } from './pages/Game'
+import { BackButton } from './components/BackButton'
 
 function App() {
   return (
     <main className='board'>
-      <h1 className='title'>Tres en raya</h1>
-      <Routes>
-        <Route path='/' element={<UsernameForm />} />
-        <Route path='/ranking' element={<Ranking />} />
-        <Route path='/game' element={<Game />} />
-      </Routes>
+      <BackButton />
+      <div className='content'>
+        <h1 className='title'>Tres en raya</h1>
+        <Routes>
+          <Route path='/' element={<UsernameForm />} />
+          <Route path='/ranking' element={<Ranking />} />
+          <Route path='/game' element={<Game />} />
+        </Routes>
+      </div>
     </main>
   )
 }
