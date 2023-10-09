4b9e5031bafc309ac99cfaf30f439d16c269b35f
Juan LB <ljuan7397@gmail.com>
2023-10-09 12:04:07 +0200

    Add disabled to square component

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,14 +1,20 @@
 export const Square = ({
   children,
   updateBoard,
-  index
+  index,
+  disabled
 }: {
   children: string,
   updateBoard: (index: number) => void,
   index: number
+  disabled: boolean
 }) => {
   return (
-    <button className='square' onClick={() => updateBoard(index)}>
+    <button
+      className='square'
+      onClick={() => updateBoard(index)}
+      disabled={disabled}
+    >
       {children}
     </button>
   )
