dbf9beaa5c554d028cd6a3a9b5a946d89805520f
Juan LB <ljuan7397@gmail.com>
2023-10-09 12:04:26 +0200

    Create a basic Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,25 +1,58 @@
+import axios from "axios"
 import { Square } from "./Square"
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { RootState } from "../store/store"
-import { setBoard } from "../store/features/gameSlice"
+import { useEffect, useState } from "react"
 
 export const Board = () => {
     const board = useSelector((state: RootState) => state.game.board)
-    const dispatch = useDispatch()
-    const updateBoard = (index: number) => {
-        const newBoard = [...board]
+    const turn = useSelector((state: RootState) => state.game.turn)
+    const game = useSelector((state: RootState) => state.game)
+
+    const [boardLocal, setBoardLocal] = useState(board)
+    const [turnLocal, setTurnLocal] = useState(turn)
+    const [status, setStatus] = useState(game.status)
+
+    useEffect(() => {
+        if (game.id !== '' && turnLocal === game.playerId) {
+            updateMove()
+        }
+    }, [boardLocal]);
+
+    const updateLocalBoard = (index: number) => {
+        setTurnLocal(game.playerId)
+        const newBoard = [...boardLocal]
         newBoard[index] = 'X'
-        dispatch(setBoard({ board: newBoard }))
+        setBoardLocal(newBoard)
+    }
+
+    const updateMove = async () => {
+        const { data: moveIA } = await axios.post('http://localhost:3000/games/move', {
+            id: game.id,
+            board: boardLocal,
+            status: game.status,
+            turn: game.turn,
+            playerId: game.playerId
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        setTurnLocal('IA')
+        setBoardLocal(moveIA.board)
+        setStatus(moveIA.status)
     }
     return (
         <section className='game'>
             {
-                board.map((_, index) => (
+                boardLocal.map((_, index) => (
                     <Square
                         key={index}
                         index={index}
-                        updateBoard={updateBoard}>
-                        {board[index]}
+                        updateBoard={updateLocalBoard}
+                        disabled={status === 'FINISHED'}
+                    >
+                        {boardLocal[index]}
                     </Square>
                 ))
             }
